d45a12f15cb9df9c7dc01566f9edd8d8006e5fef
abwmcoders <abwmcode@gmail.com>
2022-12-08 18:33:56 -0800

    ref:print with the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char my_string[] = "Programming is like building a multilingual puzzle";
+	char my_string[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(my_string);
-	return 0;
+	return (0);
 }
