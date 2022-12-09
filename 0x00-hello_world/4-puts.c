bff8a927851ca3b255f96ecf475b360cf67ba3a2
abwmcoders <abwmcode@gmail.com>
2022-12-08 18:24:53 -0800

    ref:print with the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print Programming is like building a multilingual puzzle
+ * return - returns 0
+ */
+int main(void)
+{
+	char my_string[] = "Programming is like building a multilingual puzzle";
+	puts(my_string);
+	return 0;
+}
