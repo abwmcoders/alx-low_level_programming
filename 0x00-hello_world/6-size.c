befdfa09243b17662f0d842891b12a00212ef7cc
abwmcoders <abwmcode@gmail.com>
2022-12-08 19:25:24 -0800

    ref:types in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("size of int: %ld bytes\n", sizeof(intType));
-	printf("size of float: %ld bytes\n", sizeof(floatType));
 	printf("size of char: %ld bytes\n", sizeof(charType));
+	printf("size of int: %ld bytes\n", sizeof(intType));
 	printf("size of long int: %ld bytes\n", sizeof(long int));
-	printf("sixe of long long int: %ld bytes\n", sizeof(long long int));
+	printf("size of long long int: %ld bytes\n", sizeof(long long int));
+	printf("size of float: %ld bytes\n", sizeof(floatType));
 	return (0);
 }
