b6e6cc1a91681b84cf1fa82b22836dee5d08d4e1
abwmcoders <abwmcode@gmail.com>
2022-12-08 19:18:47 -0800

    ref:betty lintrols

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 
 	printf("size of int: %ld bytes\n", sizeof(intType));
 	printf("size of float: %ld bytes\n", sizeof(floatType));
-	printf("size of double: %ld bytes\n", sizeof(doubleType));
 	printf("size of char: %ld bytes\n", sizeof(charType));
 	printf("size of long int: %ld bytes\n", sizeof(long int));
 	printf("sixe of long long int: %ld bytes\n", sizeof(long long int));
