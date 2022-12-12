e60cbe82e4b0922056be9a4f1fccfbf045492e0b
abwmcoders <abwmcode@gmail.com>
2022-12-11 16:05:56 -0800

    ref: upper and lower case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Return 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	char ALPHABET = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+	}
+	while (ALPHABET <= 'Z')
+	{
+		putchar(ALPHABET);
+	}
+	putchar('\n');
+	return (0);
+}
