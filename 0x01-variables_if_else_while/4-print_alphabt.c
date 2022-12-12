908ff443afcb262170d81db8caf825ba7c66fe23
abwmcoders <abwmcode@gmail.com>
2022-12-11 16:19:45 -0800

    ref: skip certain alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Return 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e' && alphabet == 'q')
+		{
+			continue;
+		}
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
