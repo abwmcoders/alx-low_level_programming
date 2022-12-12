55c3f222c4db0180162350b3138b69bcc0a233f7
abwmcoders <abwmcode@gmail.com>
2022-12-11 16:41:37 -0800

    ref: fix error

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Return 0
+ */
+int main(void)
+{
+	char number = 0;
+
+	while (number < 10)
+	{
+		printf("%c", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}	
