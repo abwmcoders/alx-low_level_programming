7610b970c1a5cb5fade36ce62c51f24107ffdb9e
abwmcoders <abwmcode@gmail.com>
2022-12-11 17:19:10 -0800

    ref:numbers betwen 0 and 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Return: Return 0
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = '0'; nums <= '9'; nums++)
+	{
+		putchar(nums);
+	}
+	putchar('\n');
+	return (0);
+}
