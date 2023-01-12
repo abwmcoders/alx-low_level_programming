58e5ad6eb11117e6846ad405963815f370bfee03
abwmcoders <abwmcode@gmail.com>
2023-01-11 18:55:38 -0800

    ref:completed advanced task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * **strtow - splits sring
+ * @*str: string
+ * Return: return 0
+ */
+char **strtow(char *str)
+{
+	char * splitedtoken = strtok(str, " ");
+	printf( " %s\n", splitedtoken ); //printing the token
+	return 0;
+}
