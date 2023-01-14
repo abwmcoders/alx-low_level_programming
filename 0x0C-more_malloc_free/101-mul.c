124546e2f79cfe1c8e4a9cca3f188d1a3b73c53e
abwmcoders <abwmcode@gmail.com>
2023-01-13 16:07:48 -0800

    ref:advanced task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * mul - multiply two positive integers
+ * @num1: first integer
+ * @num2: second integer
+ * @result: result of multiplication
+ * Return: new void pointer or NULL if it fails
+ */
+
+
+int mul(int result, int i, int num1, int num2) {
+    if (i<b) {
+        return Product(result+a,i+1,num1,num2);
+    } else {
+        return result;
+    }
+}
